21374a5f57ea912905bf28eebeb298adf07ad209
atanas-n-nikolov <156112757+atanas-n-nikolov@users.noreply.github.com>
2025-03-17 07:47:54 +0200

    Delete game functionality

--- client/src/components/game-details/GameDetails.jsx
+++ client/src/components/game-details/GameDetails.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 import { useParams } from "react-router";
 import gameService from "../../services/gameService";
 
 export default function GameDetails() {
+    const navigate = useNavigate();
     const [game, setGame] = useState({});
     const { gameId } = useParams();
 
@@ -12,7 +13,20 @@ export default function GameDetails() {
             const result = await gameService.getOne(gameId);
             setGame(result);
         })();
-    }, [gameId])
+    }, [gameId]);
+
+
+    const gameDeleteClickHandler = async () => {
+        const hasConfirm = confirm(`Are you sure you want to delete this ${game.title} game?`);
+
+        if(!hasConfirm) {
+            return;
+        };
+
+        await gameService.delete(gameId);
+
+        navigate('/games')
+    };
     return (
         <section id="game-details">
             <h1>Game Details</h1>
@@ -48,7 +62,7 @@ export default function GameDetails() {
                 {/* <!-- Edit/Delete buttons ( Only for creator of this game )  --> */}
                 <div className="buttons">
                     <Link to="#" className="button">Edit</Link>
-                    <Link to="#" className="button">Delete</Link>
+                    <button onClick={gameDeleteClickHandler} className="button">Delete</button>
                 </div>
             </div>
 
