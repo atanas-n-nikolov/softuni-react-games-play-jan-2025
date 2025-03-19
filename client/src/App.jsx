7160ffa3108bf3eb909ed9f9a1da2e1104e399c6
atanas-n-nikolov <156112757+atanas-n-nikolov@users.noreply.github.com>
2025-03-19 16:56:39 +0200

    Add login custom hook

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,8 +16,8 @@ import { useState } from 'react';
 export default function App() {
     const [email, setEmail] = useState('');
 
-    const userLoginHandler = (email) => {
-        setEmail(email);
+    const userLoginHandler = (authData) => {
+        setEmail(authData.email);
     }
 
     return (
